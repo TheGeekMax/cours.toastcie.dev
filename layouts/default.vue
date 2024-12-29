48f300884eb07d6240a5add94fe9e2fc707b68f7
TheGeekMax <maximesanciaume@gmail.com>
2024-12-29 09:00:46 +0100

    added kind of working list

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#offcanvasSidebar"></button>
         </div>
         <div class="offcanvas-body">
-            <ul class="nav flex-line">
+            <ul class="nav flex-line" id="sidebar">
                 <sidebarLink to="/">Home</sidebarLink>
                 <sidebarLink to="/articles">articles</sidebarLink>
                 <slot name="sidebar"/>
@@ -21,8 +21,7 @@
         </div>
     </div>
 
-    <div class="content flex-grow-1 p-3">
-        <p v-if="isArticles">oui {{ detail }}</p>
+    <div class="content flex-grow-1 p-3" id="content" ref="content">
         <slot />
     </div>
   </div>
@@ -30,31 +29,113 @@
 
 <script setup lang="ts">
 //get url
-import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const uri = route.path.split('/').reverse()
 const isArticles = uri[1] === 'articles'
-const articleName = uri[0]
-const detail = ref('')
 
-if(isArticles) {
-    const { data } = await useFetch('/api/getarticledetail/' + articleName)
-    detail.value = data
+onMounted(async () => {
+    //get the 2nd child of test
+    if(!isArticles) return
+
+    const side = document.getElementById('sidebar')
+    
+    let data = document.getElementById('content')?.querySelector('div')
+    let arrContent = Array.from(data?.children)
+    
+    const articleDetail = getArticleDetail(arrContent)
+    for (let i = 0; i < articleDetail.length; i++) {
+        let dropDown = createDropDown(articleDetail[i].name, articleDetail[i].details)
+        side?.appendChild(dropDown)
+    }
+
+})
+
+//for dropdown creation
+let createDropDown = (name :string, details : { name: string; id: string }[]) => {
+    let mainDiv = document.createElement('div')
+    mainDiv.classList.add('dropdown')
+
+    //create Name button
+    let nameButton = document.createElement('button')
+    nameButton.classList.add('btn', 'btn-secondary', 'dropdown-toggle')
+    nameButton.setAttribute('type', 'button')
+    nameButton.setAttribute('data-bs-toggle', 'dropdown')
+    nameButton.setAttribute('aria-expanded', 'false')
+    nameButton.innerText = name
+    mainDiv.appendChild(nameButton)
+
+    //create dropdown menu
+    let dropdownMenu = document.createElement('ul')
+    dropdownMenu.classList.add('dropdown-menu')
+    //dropdownMenu.setAttribute('aria-labelledby', 'dropdownMenuButton')
+    for (let i = 0; i < details.length; i++) {
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        a.classList.add('dropdown-item')
+        a.href = '#' + details[i].id
+        a.innerText = details[i].name
+        li.appendChild(a)
+        dropdownMenu.appendChild(li)
+    }
+    mainDiv.appendChild(dropdownMenu)
+    return mainDiv
 }
 
+//for tag detection
+const H2_NAME = "ProseH2.vue"
+const H3_NAME = "ProseH3.vue"
+
+let detectTag = (tag : HTMLElement) => {
+    // return { tag : TAG (H2, H3 or other), name : NAME (inner text without comments), id: ID (id of the tag)}
+    let tagName = tag.innerText
+    let tagId = tag.id
+    let tagType = tag.getAttribute('data-v-inspector')?.includes(H2_NAME) ? 'H2' : tag.getAttribute('data-v-inspector')?.includes(H3_NAME) ? 'H3' : 'OTHER'
+    return { tag : tagType, name : tagName, id: tagId }
+}
+
+function getArticleDetail(article: HTMLElement[]) : { name: string; details: { name: string; id: string }[] }[] {
+    let index = ref(0)
+    let details = ref<{ name: string; details: { name: string; id: string }[] }[]>([])
+
+    //find the first h2
+
+    for (let i = 0; i < article.length; i++) {
+        let current = detectTag(article[i])
+        if (current.tag === 'H2') {
+            index.value = i
+            break
+        }
+    }
+
+    let currentName = ref('')
+    let subDetails = ref<{ name: string; id: string }[]>([])
+    for (let i = index.value; i < article.length; i++) {
+        let current = detectTag(article[i])
+        if (current.tag === 'H2') {
+            if (currentName.value !== '') {
+                details.value.push({ name: currentName.value, details: subDetails.value })
+                subDetails.value = []
+            }
+            currentName.value = current.name
+        } else if (current.tag === 'H3') {
+            subDetails.value.push({ name: current.name, id: current.id })
+        }
+    }
+    details.value.push({ name: currentName.value, details: subDetails.value })
+    return details.value
+}
 </script>
 
+
 <style>
+    @media (max-width: 992px) {
+        .content {
+            margin-left: 0;
+        }
 
-
-@media (max-width: 992px) {
-    .content {
-        margin-left: 0;
+        #offcanvasSidebar {
+            width: 200px; /* Adjust the width of the sidebar */
+        }
     }
-
-    #offcanvasSidebar {
-        width: 200px; /* Adjust the width of the sidebar */
-    }
-}
 </style>
